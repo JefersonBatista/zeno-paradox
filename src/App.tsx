55585b24bdc3c0f18008ee11e5d7b88c09bf5926
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2025-08-09 11:17:47 -0300

    feat: position achilles and tortoise

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
 import Line from "./components/Line";
 import "./App.css";
 
 function App() {
-  const [achilles, setAchilles] = useState<number>(0);
-  const [tortoise, setTortoise] = useState<number>(100);
-
   return <Line />;
 }
 
--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import "./Line.css";
+import Achilles from "../assets/achilles.jpg";
+import Tortoise from "../assets/tortoise.jpg";
 
 function Line() {
-  return <div className="line" />;
+  const [achilles, setAchilles] = useState<number>(300);
+  const [tortoise, setTortoise] = useState<number>(950);
+
+  return (
+    <div className="line">
+      <img
+        src={Achilles}
+        alt="Achilles"
+        style={{ marginLeft: `${achilles}px` }}
+      />
+      <img
+        src={Tortoise}
+        alt="Tortoise"
+        style={{ marginRight: `calc(1000px - ${tortoise}px)` }}
+      />
+    </div>
+  );
 }
 
 export default Line;
